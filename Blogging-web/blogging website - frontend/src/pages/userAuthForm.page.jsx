eddcac08b11a6eb736e1ab473829ab1a2a63deda
pratikjetani05 <pratikjetani14@gmail.com>
2024-12-24 17:36:22 +0530

    new

--- Blogging-web/blogging website - frontend/src/pages/userAuthForm.page.jsx
+++ Blogging-web/blogging website - frontend/src/pages/userAuthForm.page.jsx
@@ -1,39 +1,101 @@
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 import InputBox from "../components/input.component";
 import googleicon from "../imgs/google.png"
 import AnimationWrapper from "../common/page-animation";
 
+import { Toaster, toast} from "react-hot-toast"
+import axios from "axios";
+import { storeInSession } from "../common/session";
+
+
 const UserAuthForm = ({type})=>{
+
+
+    const userAuthThroughServer = (serverRoute, fromData) => {
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + serverRoute, fromData)
+        .then(({data}) => {
+            storeInSession("user", JSON.stringify(data));
+            console.log(sessionStorage);
+        })
+        .catch(({response}) => {
+            toast.error(response.data.error);
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let serverRoute = type == "sign-in" ? "/signin" : "/signup";
+
+        let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
+        let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
+
+        //formData
+        let form = new FormData(formElement);
+        let formData = {};
+
+        for(let [key,value] of form.entries()){
+            formData[key] = value;
+        }
+
+        let { fullname, email, password} = formData;
+
+        //form validation 
+        if(fullname){
+            if(fullname.length < 3){
+                return toast.error("Fullname must be atleast 3 letters long");
+            }
+        }
+        if(!email.length){
+            return toast.error("Enter Email");
+        }
+        if(!emailRegex.test(email)){
+            return toast.error("Email is invalid");
+        }
+        if(!passwordRegex.test(password)){
+            return toast.error("password should be 6 to 20 character long with numeric, 1 lowercase and 1 uppercase letters");
+        }
+
+        userAuthThroughServer(serverRoute , formData);
+    }
+
     return(
+
         <AnimationWrapper keyValue={type}>
             <section className="h-cover flex items-center justify-center ">
-            <form className="w-[80%] max-w-[400px]">
+            <Toaster />
+            <form id="formElement" className="w-[80%] max-w-[400px]">
                 <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
                     {type == "sign-in" ? "Welcome Back" : "Join us today"}
                 </h1>
                 {
                     type != "sign-in" ? 
                     <InputBox
-                     name="full name"
+                     name="fullname"
                     type="text"
                     placeholder="Full Name"
                     icon="fi-rr-user"
                     /> :""
                 }
                 <InputBox
-                     name="email "
+                    name="email"
                     type="email"
                     placeholder="E-mail"
                     icon="fi-rr-envelope"
                     />
                 <InputBox
-                     name="password "
+                    name="password"
                     type="password"
                     placeholder="Password"
                     icon="fi-rr-key"
                     />
 
-                    <button className="btn-dark center mt-14" type="submit">
+                    <button 
+                        className="btn-dark center mt-14" 
+                        type="submit"
+                        onClick={handleSubmit}
+                    >
                         {type.replace("-" , " ")}
                     </button>
 
@@ -68,8 +130,6 @@ const UserAuthForm = ({type})=>{
 
         </section>
         </AnimationWrapper>
-        
-
     )
 }
 
